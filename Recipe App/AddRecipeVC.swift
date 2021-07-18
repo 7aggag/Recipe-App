43eca4336841aaf16335c4c0414919a982b44db8
Haggag <test@Haggag-MacBook-Pro.local>
2021-07-18 15:21:03 +0200

    create recipe model

--- Recipe App/AddRecipeVC.swift
+++ Recipe App/AddRecipeVC.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class AddRecipeVC: UIViewController {
+    
+    @IBOutlet weak var addrecipe : UITextField!
+    @IBOutlet weak var Listofingredients : UITextView!
+    @IBOutlet weak var Preparationsteps : UITextView!
+    @IBOutlet weak var addrecipebtn : UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    
 }
 
---
+++ Recipe App/recipe.swift
@@ -0,0 +1,13 @@
+//
+//  recipe.swift
+//  Recipe App
+//
+//  Created by Haggag on 18/07/2021.
+//
+
+import Foundation
+struct recipe {
+    var recipename : String
+    var Listofingredients : String
+    var Preparationsteps : String
+}
