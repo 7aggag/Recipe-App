7d4ca32a6211adc116a14f13369fc7d59a3ab949
Haggag <test@Haggag-MacBook-Pro.local>
2021-07-18 18:06:55 +0200

    finsh recipesvc

---
+++ Recipe App/Recipes.swift
@@ -0,0 +1,19 @@
+//
+//  Recipes.swift
+//  Recipe App
+//
+//  Created by Haggag on 18/07/2021.
+//
+
+import UIKit
+
+class Recipes: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
